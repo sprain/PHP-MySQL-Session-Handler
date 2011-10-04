9f49976e7455e97f0337588970fda2df9f11c81d
Manuel Reinhard <manu@sprain.ch>
2011-10-04 23:40:07 +0100

    initial commit
    Signed-off-by: Manuel Reinhard <manu@sprain.ch>

---
+++ SessionHandler.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+* A PHP session handler to keep session data within a MySQL database
+*
+* @author 	Manuel Reinhard <manu@sprain.ch>
+* @link		a url
+*/
+
+class SessionHandler{
+
+    /**
+     * a database MySQLi connection resource
+     * @var resource
+     */
+    protected $dbConnection;
+    
+    
+    /**
+     * the name of the DB table which handles the sessions
+     * @var string
+     */
+    protected $dbTable;
+	
+
+
+	/**
+	 * Set db data if no connection is being injected
+	 * @param 	string	$dbHost	
+	 * @param	string	$dbUser
+	 * @param	string	$dbPassword
+	 * @param	string	$dbDatabase
+	 */	
+	public function setDbDetails($dbHost, $dbUser, $dbPassword, $dbDatabase){
+
+		//create db connection
+		$this->dbConnection = new mysqli($dbHost, $dbUser, $dbPassword, $dbDatabase);
+		
+		//check connection
+		if (mysqli_connect_error()) {
+		    throw new Exception('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+		}//if
+			
+	}//function
+	
+	
+	
+	/**
+	 * Inject DB connection from outside
+	 * @param 	object	$dbConnection	expects MySQLi object
+	 */
+	public function setDbConnection($dbConnection){
+		$this->dbConnection = $dbConnection;
+	}
+	
+	
+	/**
+	 * Inject DB connection from outside
+	 * @param 	object	$dbConnection	expects MySQLi object
+	 */
+	public function setDbTable($dbTable){
+		$this->dbTable = $dbTable;
+	}
+	
+
+    /**
+     * Open the session
+     * @return bool
+     */
+    public function open() {
+  
+        //delete old session handlers
+        $limit = time() - (3600 * 24);
+        $sql = sprintf("DELETE FROM %s WHERE timestamp < %s", $this->dbTable, $limit);
+        return $this->dbConnection->query($sql);
+
+    }
+
+    /**
+     * Close the session
+     * @return bool
+     */
+    public function close() {
+
+        return $this->dbConnection->close();
+
+    }
+
+    /**
+     * Read the session
+     * @param int session id
+     * @return string string of the sessoin
+     */
+    public function read($id) {
+
+        $sql = sprintf("SELECT data FROM %s WHERE id = '%s'", $this->dbTable, $this->dbConnection->escape_string($id));
+        if ($result = $this->dbConnection->query($sql)) {
+            if ($result->num_rows && $result->num_rows > 0) {
+                $record = $result->fetch_assoc();
+                return $record['data'];
+            }
+        }
+        return '';
+    }
+    
+
+    /**
+     * Write the session
+     * @param int session id
+     * @param string data of the session
+     */
+    public function write($id, $data) {
+
+        $sql = sprintf("REPLACE INTO %s VALUES('%s', '%s', '%s')",
+        			   $this->dbTable, 
+                       $this->dbConnection->escape_string($id),
+                       $this->dbConnection->escape_string($data),
+                       time());
+        return $this->dbConnection->query($sql);
+
+    }
+
+    /**
+     * Destoroy the session
+     * @param int session id
+     * @return bool
+     */
+    public function destroy($id) {
+
+        $sql = sprintf("DELETE FROM %s WHERE `id` = '%s'", $this->dbTable, $this->dbConnection->escape_string($id));
+        return $this->dbConnection->query($sql);
+
+	}
+	
+	
+
+    /**
+     * Garbage Collector
+     * @param int life time (sec.)
+     * @return bool
+     * @see session.gc_divisor      100
+     * @see session.gc_maxlifetime 1440
+     * @see session.gc_probability    1
+     * @usage execution rate 1/100
+     *        (session.gc_probability/session.gc_divisor)
+     */
+    public function gc($max) {
+
+        $sql = sprintf("DELETE FROM %s WHERE `timestamp` < '%s'", $this->dbTable, time() - intval($max));
+        return $this->dbConnection->query($sql);
+
+    }
+
+}//class
---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+require_once('SessionHandler.php');
+
+$session = new SessionHandler();
+
+// add db data
+$session->setDbData('localhost', 'username', 'password', 'database');
+
+// OR alternatively send a MySQLi ressource
+// $session->setDbConnection($mysqli);
+
+$session->setDbTable('session_handler_table');
+session_set_save_handler(array($session, 'open'),
+                         array($session, 'close'),
+                         array($session, 'read'),
+                         array($session, 'write'),
+                         array($session, 'destroy'),
+                         array($session, 'gc'));
+session_start();
